99280f66f586ccbb380d29005793ebbd6dd4bd4d
Tilzen <lucas.nascimentoR@hotmail.com>
2022-07-04 02:39:14 -0300

    Adiciona configuração do lunarvim

---
+++ .config/lvim/config.lua
@@ -0,0 +1,61 @@
+--[[
+lvim is the global options object
+
+Linters should be
+filled in as strings with either
+a global executable or a path to
+an executable
+]]
+-- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+
+-- general
+lvim.log.level = "warn"
+lvim.format_on_save = true
+lvim.colorscheme = "onedarker"
+-- to disable icons and use a minimalist setup, uncomment the following
+-- lvim.use_icons = false
+
+-- keymappings [view all the defaults by pressing <leader>Lk]
+lvim.leader = "space"
+
+-- keymapping
+
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
+
+lvim.keys.insert_mode["<C-q>"] = ":q<cr>"
+lvim.keys.insert_mode["<C-c>"] = "+y"
+lvim.keys.insert_mode["<C-v>"] = "<C-R>+"
+lvim.keys.insert_mode["<C-x>"] = "+x"
+
+
+lvim.keys.visual_mode["<C-c>"] = "+y"
+lvim.keys.visual_mode["<C-v>"] = "<C-R>+"
+lvim.keys.visual_mode["<C-x>"] = "+x"
+
+
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.notify.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enabled = true
