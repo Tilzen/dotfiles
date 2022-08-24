3332a1bec22187710c04c0eadad47fc160ae9d18
Tilzen <lucas.nascimentoR@hotmail.com>
2022-08-23 22:55:18 -0300

    fix typo

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,7 +6,7 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "Tilzem"
+(setq user-full-name "Tilzen"
       user-mail-address "tilzen@protonmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
